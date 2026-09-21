940bf193a427e4043b1343c1f3db502c5c9202a7
medina <julian.medina.rosero@corrounivalle.edu.co>
2026-09-20 19:36:30 -0500

    implementation for various channels

--- src/bld.c
+++ src/bld.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv) {
   rebuild(argc, argv);
-  cmd("gcc $(gsl-config --cflags) -c gsl_wrapper.c image_wrapper.c main.c");
+  cmd("gcc $(gsl-config --cflags) -c gsl_wrapper.c image_wrapper.c main.c -O3 -ffast-math");
   cmd("gcc $(gsl-config --libs) -o main *.o");
   //cmd("rm *.o");
   cmd("./main");
